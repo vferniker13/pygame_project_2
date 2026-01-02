24fa88d514de0952e569277656943aa3f4c16401
code-n-cry <egor.schukiv@gmail.com>
2026-01-02 11:59:37 +0300

    getting ready for render

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ final = None
 def create_app():
     global walls
     app = Flask(__name__)
+    add_routes(app)
     walls = generate_walls(10)
     if not os.getenv("SECRET_KEY"):
         os.environ["SECRET_KEY"] = "example"
@@ -72,207 +73,210 @@ def load_user(user_id):
     return db.query(User).filter(User.id == user_id).first()
 
 
-@app.route("/", methods=["GET"])
-def index():
-    return render_template("index.html", user=current_user)
-
-
-@app.route("/register", methods=["GET", "POST"])
-def register():
-    if request.method == "GET":
-        return render_template("register.html", user=current_user)
-    if request.method == "POST":
-        db = next(get_db())
-        data = request.form
-        if data["password1"] == data["password2"]:
-            user = User()
-            user.username = data["username"]
-            user.hashed_password = hash_password(data["password1"])
-            user.color = generate_random_color()
-            db.add(user)
-            db.commit()
-            return redirect("/")
-        return render_template(
-            "register.html", user=current_user, error="Пароли не совпадают!"
-        )
-
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    if request.method == "GET":
-        return render_template("login.html", user=current_user)
-    if request.method == "POST":
-        db = next(get_db())
-        data = request.form
-        if db.query(User).filter(User.username == data["username"]).first():
-            user = (
-                db.query(User)
-                .filter(User.username == data["username"])
-                .first()
-            )
-            if verify_password(data["password1"], user.hashed_password):
-                login_user(user)
+def add_routes(app):
+    @app.route("/", methods=["GET"])
+    def index():
+        return render_template("index.html", user=current_user)
+    
+    
+    @app.route("/register", methods=["GET", "POST"])
+    def register():
+        if request.method == "GET":
+            return render_template("register.html", user=current_user)
+        if request.method == "POST":
+            db = next(get_db())
+            data = request.form
+            if data["password1"] == data["password2"]:
+                user = User()
+                user.username = data["username"]
+                user.hashed_password = hash_password(data["password1"])
+                user.color = generate_random_color()
+                db.add(user)
+                db.commit()
                 return redirect("/")
             return render_template(
-                "login.html", user=current_user, error="Неверный пароль!"
+                "register.html", user=current_user, error="Пароли не совпадают!"
             )
-        return render_template(
-            "login.html", user=current_user, error="Пользователя не существует"
-        )
-
-
-@app.route("/profile", methods=["GET", "POST"])
-def profile():
-    if request.method == "GET":
-        db = next(get_db())
-        user = db.query(User).filter(User.id == current_user.id).first()
-        return render_template("profile.html", user=user)
-    if request.method == "POST":
-        db = next(get_db())
-        data = request.form
-        user = db.query(User).filter(User.id == current_user.id).first()
-        user.username = data["username"]
-        user.color = data["color"]
-        db.add(user)
-        db.commit()
-        return render_template("profile.html", user=user)
-
-
-@app.route("/select/hunter/<sid>")
-def become_hunter(sid: str):
-    global players, info, round_in_proccess
-    if not round_in_proccess and info["total_hunters"] < info["max_hunters"]:
-        if not current_user.is_anonymous:
-            players[sid]["role"] = "hunter"
+    
+    
+    @app.route("/login", methods=["GET", "POST"])
+    def login():
+        if request.method == "GET":
+            return render_template("login.html", user=current_user)
+        if request.method == "POST":
+            db = next(get_db())
+            data = request.form
+            if db.query(User).filter(User.username == data["username"]).first():
+                user = (
+                    db.query(User)
+                    .filter(User.username == data["username"])
+                    .first()
+                )
+                if verify_password(data["password1"], user.hashed_password):
+                    login_user(user)
+                    return redirect("/")
+                return render_template(
+                    "login.html", user=current_user, error="Неверный пароль!"
+                )
+            return render_template(
+                "login.html", user=current_user, error="Пользователя не существует"
+            )
+    
+    
+    @app.route("/profile", methods=["GET", "POST"])
+    def profile():
+        if request.method == "GET":
+            db = next(get_db())
+            user = db.query(User).filter(User.id == current_user.id).first()
+            return render_template("profile.html", user=user)
+        if request.method == "POST":
+            db = next(get_db())
+            data = request.form
+            user = db.query(User).filter(User.id == current_user.id).first()
+            user.username = data["username"]
+            user.color = data["color"]
+            db.add(user)
+            db.commit()
+            return render_template("profile.html", user=user)
+    
+    
+    @app.route("/select/hunter/<sid>")
+    def become_hunter(sid: str):
+        global players, info, round_in_proccess
+        if not round_in_proccess and info["total_hunters"] < info["max_hunters"]:
+            if not current_user.is_anonymous:
+                players[sid]["role"] = "hunter"
+                socket.emit("update_all", players)
+                info["total_hunters"] += 1
+                info["total_survivors"] -= 1
+                players["hunter"] = sid
+                socket.emit("update_info", info)
+                if info["total_survivors"] <= 0:
+                    return {"status": 406}
+                thread = threading.Thread(target=wait_to_game)
+                thread.start()
+                return {"status": 200}
+            else:
+                return {"status": 401}
+        return {"status": 403}
+    
+    
+    @app.route("/select/survivor/<sid>")
+    def become_survivor(sid: str):
+        global players, info, round_in_proccess
+        if (
+            not round_in_proccess
+            and players[sid]["role"] == "hunter"
+            and info["total_hunters"] > 0
+        ):
+            players["hunter"] = None
+            info["total_hunters"] -= 1
+        if players[sid]["role"] != "survivor":
+            players[sid]["role"] = "survivor"
+            info["total_survivors"] += 1
             socket.emit("update_all", players)
-            info["total_hunters"] += 1
-            info["total_survivors"] -= 1
-            players["hunter"] = sid
             socket.emit("update_info", info)
-            if info["total_survivors"] <= 0:
-                return {"status": 406}
-            thread = threading.Thread(target=wait_to_game)
-            thread.start()
             return {"status": 200}
-        else:
-            return {"status": 401}
-    return {"status": 403}
+        return {"status": 403}
+    
 
-
-@app.route("/select/survivor/<sid>")
-def become_survivor(sid: str):
-    global players, info, round_in_proccess
-    if (
-        not round_in_proccess
-        and players[sid]["role"] == "hunter"
-        and info["total_hunters"] > 0
-    ):
-        players["hunter"] = None
-        info["total_hunters"] -= 1
-    if players[sid]["role"] != "survivor":
-        players[sid]["role"] = "survivor"
-        info["total_survivors"] += 1
-        socket.emit("update_all", players)
-        socket.emit("update_info", info)
-        return {"status": 200}
-    return {"status": 403}
+    @app.route("/logout")
+    @login_required
+    def logout():
+        logout_user()
+        return redirect("/")
 
 
 @socket.on("connect")
 def on_connect():
-    global players, info, walls, round_in_proccess
-    username = f"Гость{info['total_survivors'] + info['total_hunters']}"
-    if not current_user.is_anonymous:
-        username = current_user.username
-    players[request.sid] = {
-        "username": username,
-        "x": randint(0, 750),
-        "y": randint(0, 750),
-        "color": generate_random_color(),
-        "role": "survivor",
-        "is_alive": True if not round_in_proccess else False,
-    }
-    info["total_survivors"] += 1
-    if not current_user.is_anonymous:
-        players[request.sid]["color"] = current_user.color
-    socket.emit("ur_sid", {"id": request.sid}, to=request.sid)
-    socket.emit("update_all", players)
-    socket.emit("update_walls", walls)
-    socket.emit("update_info", info)
-
-
+        global players, info, walls, round_in_proccess
+        username = f"Гость{info['total_survivors'] + info['total_hunters']}"
+        if not current_user.is_anonymous:
+            username = current_user.username
+        players[request.sid] = {
+            "username": username,
+            "x": randint(0, 750),
+            "y": randint(0, 750),
+            "color": generate_random_color(),
+            "role": "survivor",
+            "is_alive": True if not round_in_proccess else False,
+        }
+        info["total_survivors"] += 1
+        if not current_user.is_anonymous:
+            players[request.sid]["color"] = current_user.color
+        socket.emit("ur_sid", {"id": request.sid}, to=request.sid)
+        socket.emit("update_all", players)
+        socket.emit("update_walls", walls)
+        socket.emit("update_info", info)
+    
+    
 @socket.on("kill")
 def kill_player(data: dict):
-    global players, info, round_in_proccess
-    if (
-        round_in_proccess is True
-        and players[data["target_id"]] != players["hunter"]
-    ):
-        players[data["target_id"]]["is_alive"] = False
-        info["total_survivors"] -= 1
-        hunter = players["hunter"]
-        username = players[hunter]["username"]
-        db = next(get_db())
-        user = db.query(User).filter(User.username == username).first()
-        user.kills += 1
-        db.add(user)
-        db.commit()
-        socket.emit("kill_signal", data)
-        socket.emit("update_info", info)
-    stop_timer()
-    socket.emit("update_all", players)
+        global players, info, round_in_proccess
+        if (
+            round_in_proccess is True
+            and players[data["target_id"]] != players["hunter"]
+        ):
+            players[data["target_id"]]["is_alive"] = False
+            info["total_survivors"] -= 1
+            hunter = players["hunter"]
+            username = players[hunter]["username"]
+            db = next(get_db())
+            user = db.query(User).filter(User.username == username).first()
+            user.kills += 1
+            db.add(user)
+            db.commit()
+            socket.emit("kill_signal", data)
+            socket.emit("update_info", info)
+        stop_timer()
+        socket.emit("update_all", players)
+    
 
 
 @socket.on("disconnect")
 def on_disconnect():
-    global players, info
-    if players[request.sid]["role"] == "hunter":
-        info["total_hunters"] -= 1
-    else:
-        info["total_survivors"] -= 1
-    players[request.sid]["role"] = "survivor"
-    players.pop(request.sid, None)
-    socket.emit("update_all", players)
-    socket.emit("update_info", info)
+        global players, info
+        if players[request.sid]["role"] == "hunter":
+            info["total_hunters"] -= 1
+        else:
+            info["total_survivors"] -= 1
+        players[request.sid]["role"] = "survivor"
+        players.pop(request.sid, None)
+        socket.emit("update_all", players)
+        socket.emit("update_info", info)
+        
 
-
-@app.route("/logout")
-@login_required
-def logout():
-    logout_user()
-    return redirect("/")
-
-
+    
 @socket.on("move")
 def on_move(data: dict):
-    global walls
-    if request.sid in players:
-        if (
-            not is_obstacle_in_the_way(walls, data["x"], data["y"])
-            and data["x"] + 10 < 800
-            and data["x"] - 10 > 0
-            and data["y"] + 10 < 800
-            and data["y"] - 10 > 0
-        ):
-            players[request.sid]["x"] = data["x"]
-            players[request.sid]["y"] = data["y"]
-        socket.emit("update_all", players)
-
-
+        global walls
+        if request.sid in players:
+            if (
+                not is_obstacle_in_the_way(walls, data["x"], data["y"])
+                and data["x"] + 10 < 800
+                and data["x"] - 10 > 0
+                and data["y"] + 10 < 800
+                and data["y"] - 10 > 0
+            ):
+                players[request.sid]["x"] = data["x"]
+                players[request.sid]["y"] = data["y"]
+            socket.emit("update_all", players)
+    
+    
 @socket.on("check_shot")
 def on_shot(data: dict):
-    global players, round_in_proccess
-    for wall in walls:
-        data_end = [data["shot_x"], data["shot_y"]]
-        hunter = [
-            players[players["hunter"]]["x"],
-            players[players["hunter"]]["y"],
-        ]
-        wall = walls.get(wall)
-        if is_wall_on_the_line(hunter, data_end, wall):
-            return
-    for id in players:
-        if (
+        global players, round_in_proccess
+        for wall in walls:
+            data_end = [data["shot_x"], data["shot_y"]]
+            hunter = [
+                players[players["hunter"]]["x"],
+                players[players["hunter"]]["y"],
+            ]
+            wall = walls.get(wall)
+            if is_wall_on_the_line(hunter, data_end, wall):
+                return
+        for id in players:
+            if (
             players[id]
             and not isinstance(players[id], str)
             and id != players["hunter"]
